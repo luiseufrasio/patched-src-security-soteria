e725f5f54d229595eab3074043a3f81ce81abee2
Patrik Dudits <patrik.dudits@payara.fish>
2022-11-10 17:05:37 +0100

    FISH-6598: Remove no longer existing mechanism mappings, clean JASPIC Bridge
    Instead of storing mappings as classes, which might or might not be available, store
    only class names.
    Cache classes in thread safe manner.
    Consistently use configured parameter, also in cleanSubject.

--- impl/src/main/java/org/glassfish/soteria/mechanisms/jaspic/HttpBridgeServerAuthModule.java
+++ impl/src/main/java/org/glassfish/soteria/mechanisms/jaspic/HttpBridgeServerAuthModule.java
@@ -40,11 +40,10 @@
 
 package org.glassfish.soteria.mechanisms.jaspic;
 
-import org.glassfish.soteria.cdi.spi.CDIPerRequestInitializer;
-import org.glassfish.soteria.mechanisms.BasicAuthenticationMechanism;
-import org.glassfish.soteria.mechanisms.CustomFormAuthenticationMechanism;
-import org.glassfish.soteria.mechanisms.FormAuthenticationMechanism;
-import org.glassfish.soteria.mechanisms.HttpMessageContextImpl;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 import javax.enterprise.inject.spi.CDI;
 import javax.security.auth.Subject;
@@ -61,9 +60,12 @@ import javax.security.enterprise.authentication.mechanism.http.HttpAuthenticatio
 import javax.security.enterprise.authentication.mechanism.http.HttpMessageContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Logger;
+
+import org.glassfish.soteria.cdi.spi.CDIPerRequestInitializer;
+import org.glassfish.soteria.mechanisms.BasicAuthenticationMechanism;
+import org.glassfish.soteria.mechanisms.CustomFormAuthenticationMechanism;
+import org.glassfish.soteria.mechanisms.FormAuthenticationMechanism;
+import org.glassfish.soteria.mechanisms.HttpMessageContextImpl;
 
 import static javax.security.enterprise.AuthenticationStatus.NOT_DONE;
 import static javax.security.enterprise.AuthenticationStatus.SEND_FAILURE;
@@ -71,178 +73,160 @@ import static org.glassfish.soteria.mechanisms.jaspic.Jaspic.fromAuthenticationS
 import static org.glassfish.soteria.mechanisms.jaspic.Jaspic.setLastAuthenticationStatus;
 
 /**
- *
  * @author Arjan Tijms
- *
  */
 public class HttpBridgeServerAuthModule implements ServerAuthModule {
 
-    private static final Logger LOGGER =  Logger.getLogger(HttpBridgeServerAuthModule.class.getName());
+    private final static Map<String, String> mappings = new HashMap<>();
 
-        private CallbackHandler handler;
-        private final Class<?>[] supportedMessageTypes = new Class[] { HttpServletRequest.class, HttpServletResponse.class };
-        private final CDIPerRequestInitializer cdiPerRequestInitializer;
-        private final Map<String, Class<?>> mappings = new HashMap<>();
-
-        public HttpBridgeServerAuthModule(CDIPerRequestInitializer cdiPerRequestInitializer) {
-            this.cdiPerRequestInitializer = cdiPerRequestInitializer;
-            try {
-                initMappings();
-            } catch (ClassNotFoundException e) {
-                LOGGER.warning(e.getMessage());
-            }
-        }
-
-    private void initMappings() throws ClassNotFoundException {
-        mappings.put("Basic", BasicAuthenticationMechanism.class);
-        mappings.put("Form", FormAuthenticationMechanism.class);
-        mappings.put("CustomForm", CustomFormAuthenticationMechanism.class);
-        mappings.put("JWT", Thread.currentThread().getContextClassLoader().loadClass("fish.payara.microprofile.jwtauth.eesecurity.JWTAuthenticationMechanism"));
-        mappings.put("Certificate", Thread.currentThread().getContextClassLoader().loadClass("fish.payara.security.realm.mechanisms.CertificateAuthenticationMechanism"));
-        mappings.put("Azure", Thread.currentThread().getContextClassLoader().loadClass("fish.payara.security.openid.azure.AzureOpenIdAuthenticationMechanism"));
-        mappings.put("Google", Thread.currentThread().getContextClassLoader().loadClass("fish.payara.security.openid.google.GoogleOpenIdAuthenticationMechanism"));
-        mappings.put("OAuth2", Thread.currentThread().getContextClassLoader().loadClass("fish.payara.security.oauth2.OAuth2AuthenticationMechanism"));
-        mappings.put("OIDC", Thread.currentThread().getContextClassLoader().loadClass("fish.payara.security.openid.OpenIdAuthenticationMechanism"));
-        mappings.put("TwoIdentityStore", Thread.currentThread().getContextClassLoader().loadClass("fish.payara.security.authentication.twoIdentityStore.TwoIdentityStoreAuthenticationMechanism"));
+    static {
+        mappings.put("Basic", BasicAuthenticationMechanism.class.getName());
+        mappings.put("Form", FormAuthenticationMechanism.class.getName());
+        mappings.put("CustomForm", CustomFormAuthenticationMechanism.class.getName());
+        mappings.put("JWT", "fish.payara.microprofile.jwtauth.eesecurity.JWTAuthenticationMechanism");
+        mappings.put("Certificate", "fish.payara.security.realm.mechanisms.CertificateAuthenticationMechanism");
+        mappings.put("OAuth2", "fish.payara.security.oauth2.OAuth2AuthenticationMechanism");
+        mappings.put("OIDC", "fish.payara.security.openid.OpenIdAuthenticationMechanism");
+        mappings.put("TwoIdentityStore", "fish.payara.security.authentication.twoIdentityStore.TwoIdentityStoreAuthenticationMechanism");
     }
 
-        @Override
-        public void initialize(MessagePolicy requestPolicy, MessagePolicy responsePolicy, CallbackHandler handler, @SuppressWarnings("rawtypes") Map options) throws AuthException {
-            this.handler = handler;
-            // options not supported.
+    private final Class<?>[] supportedMessageTypes = new Class[]{HttpServletRequest.class, HttpServletResponse.class};
+
+    private final CDIPerRequestInitializer cdiPerRequestInitializer;
+
+    private final Map<String, Class<? extends HttpAuthenticationMechanism>> mechanismClassCache = new ConcurrentHashMap<>(3);
+
+    private CallbackHandler handler;
+
+    public HttpBridgeServerAuthModule(CDIPerRequestInitializer cdiPerRequestInitializer) {
+        this.cdiPerRequestInitializer = cdiPerRequestInitializer;
+    }
+
+    @Override
+    public void initialize(MessagePolicy requestPolicy, MessagePolicy responsePolicy, CallbackHandler handler,
+                           @SuppressWarnings("rawtypes") Map options) throws AuthException {
+        this.handler = handler;
+        // options not supported.
+    }
+
+    /**
+     * A Servlet Container Profile compliant implementation should return HttpServletRequest and HttpServletResponse, so
+     * the delegation class {@link ServerAuthContext} can choose the right SAM to delegate to.
+     */
+    @Override
+    public Class<?>[] getSupportedMessageTypes() {
+        return supportedMessageTypes;
+    }
+
+    @Override
+    public AuthStatus validateRequest(MessageInfo messageInfo, Subject clientSubject, Subject serviceSubject) throws AuthException {
+
+        HttpMessageContext msgContext = new HttpMessageContextImpl(handler, messageInfo, clientSubject);
+
+        if (cdiPerRequestInitializer != null) {
+            cdiPerRequestInitializer.init(msgContext.getRequest());
         }
 
-        /**
-         * A Servlet Container Profile compliant implementation should return HttpServletRequest and HttpServletResponse, so
-         * the delegation class {@link ServerAuthContext} can choose the right SAM to delegate to.
-         */
-        @Override
-        public Class<?>[] getSupportedMessageTypes() {
-            return supportedMessageTypes;
+        AuthenticationStatus status = NOT_DONE;
+        setLastAuthenticationStatus(msgContext.getRequest(), status);
+
+        try {
+            HttpAuthenticationMechanism mechanismInstance = getMechanism(msgContext);
+            status = mechanismInstance
+                    .validateRequest(
+                            msgContext.getRequest(),
+                            msgContext.getResponse(),
+                            msgContext);
+        } catch (AuthenticationException e) {
+            // In case of an explicit AuthException, status will
+            // be set to SEND_FAILURE, for any other (non checked) exception
+            // the status will be the default NOT_DONE
+            setLastAuthenticationStatus(msgContext.getRequest(), SEND_FAILURE);
+            throw (AuthException) new AuthException("Authentication failure in HttpAuthenticationMechanism").initCause(e);
         }
 
-        @Override
-        public AuthStatus validateRequest(MessageInfo messageInfo, Subject clientSubject, Subject serviceSubject) throws AuthException {
+        setLastAuthenticationStatus(msgContext.getRequest(), status);
 
-            HttpMessageContext msgContext = new HttpMessageContextImpl(handler, messageInfo, clientSubject);
+        return fromAuthenticationStatus(status);
+    }
 
-            if (cdiPerRequestInitializer != null) {
-                cdiPerRequestInitializer.init(msgContext.getRequest());
-            }
+    private HttpAuthenticationMechanism getMechanism(HttpMessageContext ctx) throws AuthException {
+        String mechanism = getMechanismName(ctx.getRequest());
+        Class<? extends HttpAuthenticationMechanism> mechanismClass = findMechanismClass(mechanism);
+        return CDI.current().select(mechanismClass).get();
+    }
 
-            AuthenticationStatus status = NOT_DONE;
-            setLastAuthenticationStatus(msgContext.getRequest(), status);
-
-            // Define which HttpAuthenticationMechanism we need to look for
-            HttpServletRequest request = msgContext.getRequest();
-            String mechanism = getMechanism(request);
-
-            Class<?> mechanismClassName;
-            try {
-                mechanismClassName = defineMechanismClassName(mechanism);
-            } catch (ClassNotFoundException e) {
-                throw new AuthException(e.getMessage());
-            }
-
-            try {
-                HttpAuthenticationMechanism mechanismInstance = (HttpAuthenticationMechanism) CDI.current()
-                        .select(mechanismClassName).get();
-                status = mechanismInstance
-                            .validateRequest(
-                                msgContext.getRequest(),
-                                msgContext.getResponse(),
-                                msgContext);
-            } catch (AuthenticationException e) {
-                // In case of an explicit AuthException, status will
-                // be set to SEND_FAILURE, for any other (non checked) exception
-                // the status will be the default NOT_DONE
-                setLastAuthenticationStatus(msgContext.getRequest(), SEND_FAILURE);
-                throw (AuthException) new AuthException("Authentication failure in HttpAuthenticationMechanism").initCause(e);
-            }
-            
-            setLastAuthenticationStatus(msgContext.getRequest(), status);
-            
-            return fromAuthenticationStatus(status);
-        }
-
-        @Override
-        public AuthStatus secureResponse(MessageInfo messageInfo, Subject serviceSubject) throws AuthException {
-            HttpMessageContext msgContext = new HttpMessageContextImpl(handler, messageInfo, null);
-
-            // Define which HttpAuthenticationMechanism we need to look for
-            HttpServletRequest request = msgContext.getRequest();
-            String mechanism = getMechanism(request);
-
-            Class<?> mechanismClassName;
-            try {
-                mechanismClassName = defineMechanismClassName(mechanism);
-            } catch (ClassNotFoundException e) {
-                throw new AuthException(e.getMessage());
-            }
-
-            try {
-                HttpAuthenticationMechanism mechanismInstance = (HttpAuthenticationMechanism) CDI.current()
-                        .select(mechanismClassName).get();
-                AuthenticationStatus status = mechanismInstance
-                                                 .secureResponse(
-                                                     msgContext.getRequest(),
-                                                     msgContext.getResponse(),
-                                                     msgContext);
-                AuthStatus authStatus = fromAuthenticationStatus(status);
-                if (authStatus == AuthStatus.SUCCESS) {
-                    return AuthStatus.SEND_SUCCESS;
-                }
-                return authStatus;
-            } catch (AuthenticationException e) {
-                throw (AuthException) new AuthException("Secure response failure in HttpAuthenticationMechanism").initCause(e);
-            } finally {
-                if (cdiPerRequestInitializer != null) {
-                    cdiPerRequestInitializer.destroy(msgContext.getRequest());
-                }
-            }
-
-        }
-
-        /**
-         * Called in response to a {@link HttpServletRequest#logout()} call.
-         *
-         */
-        @Override
-        public void cleanSubject(MessageInfo messageInfo, Subject subject) throws AuthException {
-            HttpMessageContext msgContext = new HttpMessageContextImpl(handler, messageInfo, subject);
-
-            CDI.current()
-               .select(HttpAuthenticationMechanism.class).get()
-               .cleanSubject(msgContext.getRequest(), msgContext.getResponse(), msgContext);
-        }
+    private String getMechanismName(HttpServletRequest request) {
+        return request.getServletContext().getInitParameter("fish.payara.security.mechanism");
+    }
 
     /**
      * Define the HttpAuthenticationMechanism to use. based on the 'fish.payara.security.mechanism' context parameter if specified.
-     * // TODO Should this be
+     *
      * @param mechanism
      * @return
      * @throws ClassNotFoundException
      */
-    private Class<?> defineMechanismClassName(String mechanism) throws ClassNotFoundException {
-        Class<?> result = mappings.get(mechanism);
-        if (result == null) {
-            if (mechanism != null) {
-                // We assume a FQCN is specified
-                result = Thread.currentThread().getContextClassLoader().loadClass(mechanism);
-                if (!HttpAuthenticationMechanism.class.isAssignableFrom(result)) {
-                    throw new IllegalArgumentException("The value for the 'fish.payara.security.mechanism' context parameter doesn't contain a reference to a HttpAuthenticationMechanism implementation.");
-                }
-                // Keep in the mappings for next requests
-                mappings.put(mechanism, result);
-            } else {
-                result = HttpAuthenticationMechanism.class;  // We assume only 1 HttpAuthenticationMechanism is defined.
-            }
+    private Class<? extends HttpAuthenticationMechanism> findMechanismClass(String mechanism) throws AuthException {
+        if (mechanism == null) {
+            return HttpAuthenticationMechanism.class;
         }
-        return result;
+        String mappedName = mappings.getOrDefault(mechanism, mechanism);
+        try {
+            return mechanismClassCache.computeIfAbsent(mappedName, this::loadMechanismClass);
+        } catch (RuntimeException e) {
+            throw new AuthException(e.getMessage());
+        }
+
     }
 
-    private String getMechanism(HttpServletRequest request) {
-        return request.getServletContext().getInitParameter("fish.payara.security.mechanism");
+    private Class<? extends HttpAuthenticationMechanism> loadMechanismClass(String mechanism) {
+        try {
+            Class<?> mechanismClass = Thread.currentThread().getContextClassLoader().loadClass(mechanism);
+            if (HttpAuthenticationMechanism.class.isAssignableFrom(mechanismClass)) {
+                return (Class<? extends HttpAuthenticationMechanism>) mechanismClass;
+            } else {
+                throw new IllegalArgumentException("Provided authentication class does not implement HttpAuthentication Mechanism: " + mechanism);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("HTTP Authentication mechanism class not found " + e.getMessage());
+        }
+    }
+
+    @Override
+    public AuthStatus secureResponse(MessageInfo messageInfo, Subject serviceSubject) throws AuthException {
+        HttpMessageContext msgContext = new HttpMessageContextImpl(handler, messageInfo, null);
+
+        try {
+            HttpAuthenticationMechanism mechanismInstance = getMechanism(msgContext);
+            AuthenticationStatus status = mechanismInstance
+                    .secureResponse(
+                            msgContext.getRequest(),
+                            msgContext.getResponse(),
+                            msgContext);
+            AuthStatus authStatus = fromAuthenticationStatus(status);
+            if (authStatus == AuthStatus.SUCCESS) {
+                return AuthStatus.SEND_SUCCESS;
+            }
+            return authStatus;
+        } catch (AuthenticationException e) {
+            throw (AuthException) new AuthException("Secure response failure in HttpAuthenticationMechanism").initCause(e);
+        } finally {
+            if (cdiPerRequestInitializer != null) {
+                cdiPerRequestInitializer.destroy(msgContext.getRequest());
+            }
+        }
+
+    }
+
+    /**
+     * Called in response to a {@link HttpServletRequest#logout()} call.
+     */
+    @Override
+    public void cleanSubject(MessageInfo messageInfo, Subject subject) throws AuthException {
+        HttpMessageContext msgContext = new HttpMessageContextImpl(handler, messageInfo, subject);
+
+        getMechanism(msgContext).cleanSubject(msgContext.getRequest(), msgContext.getResponse(), msgContext);
     }
 
 }
